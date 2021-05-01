b81b74100d22e02b9920e4fcee92d0ae476f57b9
frank <frank.d@fractal.ai>
2021-05-01 19:30:41 +0530

    updated environments/dev/vpc.tf

--- environments/dev/vpc.tf
+++ environments/dev/vpc.tf
@@ -6,7 +6,7 @@ resource "google_compute_network" "vpc" {
 
 # Subnet
 resource "google_compute_subnetwork" "subnet" {
-  name          = "${var.project_id}-subnet"
+  name          = "gke-subnet"
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
