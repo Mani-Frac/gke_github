b384d3b3d3201223eca63bd69eff5aa5a5dadd81
Manikanta Allam <manikanta.allam@fractal.ai>
2021-05-05 12:39:39 +0530

    backend CICD

--- backend/run.py
+++ backend/run.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'fourth try!! uPDATED'
+    return 'Backend Code Updated'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
