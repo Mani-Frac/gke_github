e98d3e63585760bf7a04e7ec02a3da16d0a0ce30
mani allam <mani.allam29@gmail.com>
2021-05-07 11:15:10 +0000

    terraform file

--- environments/dev/03_terraform_state_bucket.tf
+++ environments/dev/03_terraform_state_bucket.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket = "github-actions-poc-gke-05-gh-actions-gke-tfstate"
+    bucket = "github-actions-poc-gke-07-gh-actions-gke-tfstate"
     prefix = "env/dev"
   }
 }
